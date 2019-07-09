b7bdaaaf0dde7117036bd7e57e2f51fe07404fcc
Noah Shillington <noah.shillington@gmail.com>
2019-07-09 08:33:25 -0700

    Updated GitGist Link

--- Resume/Handlers/NetworkHandler.swift
+++ Resume/Handlers/NetworkHandler.swift
@@ -10,7 +10,7 @@ import UIKit
 
 struct NetworkHandler {
     
-    private let profileURL = URL(string: "https://gist.githubusercontent.com/nshillin/3ee62caea9addc5de056eff2a4a48fce/raw/e28b3f69e5b9487634bf9fa1795146d181444989/Resume.json")!
+    private let profileURL = URL(string: "https://gist.githubusercontent.com/nshillin/3ee62caea9addc5de056eff2a4a48fce/raw")!
     
     func getProfile(success:@escaping (Profile)->(), failure:@escaping (String)->()) {
         let session = URLSession.shared
